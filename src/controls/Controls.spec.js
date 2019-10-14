11621d37f0da747b77a2ab281f22f6b7367986dc
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-14 17:05:31 -0400

    Added tests for button to close gate and for text to change when gate closed

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,6 +1,6 @@
 // Test away!
 import React from 'react';
-import {render} from "@testing-library/react"
+import { render, fireEvent } from "@testing-library/react"
 import Controls from './Controls';
 
 
@@ -13,5 +13,49 @@ test
 
 test('it renders correctly', () => {
     expect(render(<Controls />)).toMatchSnapshot();
-}); 
+});
+
+test('Displays if gate locked', () => {
+    const { getByText } = render(<Controls />);
+
+    getByText(/lock/i)
+
+
+
+})
+
+
+test('Displays if gate close', () => {
+    const { getByText } = render(<Controls />);
+
+
+
+    getByText(/close/i)
+
+})
+
+
+test('if gate is locked, close button disabled', () => {
+    const { getByText } = render(<Controls locked={true} />);
+
+
+
+    expect(getByText(/close/i)).toBeFalsy;
+
+})
+
+
+test('button click closes gate', () => {
+    const toggleClosedMock = jest.fn();
+    const { getByText } = render(<Controls toggleClosed={toggleClosedMock} />);
+
+
+
+    fireEvent.click(getByText(/close/i));
+    expect(toggleClosedMock).toHaveBeenCalled();
+
+
+
+
+})
 
