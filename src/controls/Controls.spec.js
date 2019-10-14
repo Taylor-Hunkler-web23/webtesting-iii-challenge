4b2e906f291dc0c465a2de52f59438ab32103b77
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-14 15:47:39 -0400

    Added snapshot test in Controls.js

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,17 @@
 // Test away!
+import React from 'react';
+import {render} from "@testing-library/react"
+import Controls from './Controls';
+
+
+
+// test ('App renders without crashing', () => {
+//     render(<Controls/>);
+//   })
+
+test
+
+test('it renders correctly', () => {
+    expect(render(<Controls />)).toMatchSnapshot();
+}); 
+
